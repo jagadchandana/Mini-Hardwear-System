7ce397bef28c04d627fc9f135e67e7457008f268
Jagad Chandana <jagadchandana61@gmail.com>
2021-12-07 21:40:53 +0530

    Initial commit

---
+++ src/controllers/DashBordController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class DashBordController {
+    public Label lblHello;
+    public Label lblTime;
+    public Label lblDate;
+    public JFXTextField txtUserName;
+    public JFXPasswordField txtPassword;
+
+    public void customerOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void toolsOnMouseClickd(MouseEvent mouseEvent) {
+    }
+
+    public void borrowOnMouseClickd(MouseEvent mouseEvent) {
+    }
+
+    public void exitOnAction(ActionEvent actionEvent) {
+    }
+}
